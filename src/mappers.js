ec44a1cf9af2e7691dbda90cfa382837c0713ae1
Yury Lavrukhin <yurka7321@gmail.com>
2018-08-28 15:47:35 +0600

    Решение 4 ошибки

--- src/mappers.js
+++ src/mappers.js
@@ -8,7 +8,7 @@ const mapServerData = (serverData) => {
       geometry: 
       {
         type: "Point",
-        coordinates: [obj.long, obj.lat]
+        coordinates: [obj.lat, obj.long]
       },
       properties: {
         iconCaption: obj.serialNumber
